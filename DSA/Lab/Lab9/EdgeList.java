a146400260cf06d84265a27fe88efffa1a131e3e
Pham Van Phuc <phucpham.1803@gmail.com>
2023-11-16 10:43:43 +0700

    test update

--- DSA/Lab/Lab9/EdgeList.java
+++ DSA/Lab/Lab9/EdgeList.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.util.Vector;
 
 public class EdgeList{
@@ -17,24 +16,4 @@ public class EdgeList{
             System.out.println(edges.get(i));
         }
     }
-=======
-import java.util.Vector;
-
-public class EdgeList{
-    private Vector <IntegerTriple > edges;
-
-    public EdgeList(){
-        edges = new Vector <IntegerTriple >();
-    }   
-
-    public void addEdge(int w, int u, int v){
-        edges.add(new IntegerTriple(w,u,v));
-    }
-
-    public void printGraph(){
-        for(int i = 0; i < edges.size(); i++){
-            System.out.println(edges.get(i));
-        }
-    }
->>>>>>> 24c5249d655fa027d636c3c18201d8b99830bb02
 }
--- DSA/Lab/Lab9/IntegerTriple.java
+++ DSA/Lab/Lab9/IntegerTriple.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class IntegerTriple{
     private Integer weight;
     private Integer source;
@@ -26,32 +25,3 @@ class IntegerTriple{
     }
 }
 
-=======
-class IntegerTriple{
-    private Integer weight;
-    private Integer source;
-    private Integer dest;
-
-    public IntegerTriple(Integer w, Integer s, Integer d){
-        weight = w;
-        source = s;
-        dest = d;
-    }
-
-    public Integer getWeight(){
-        return weight;
-    }
-    public Integer getSource(){
-        return source;
-    }
-
-    public Integer getDest(){
-        return dest;
-    }
-
-    public String toString(){
-        return weight + " " + source + " " + dest;
-    }
-}
-
->>>>>>> 24c5249d655fa027d636c3c18201d8b99830bb02
