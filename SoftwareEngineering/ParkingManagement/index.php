9b248a657dc5579d7cde85927a57c311d2d75ec3
Pham Van Phuc <121589931+pkucpkam@users.noreply.github.com>
2024-06-05 13:17:51 +0700

    Add files via upload

---
+++ SoftwareEngineering/ParkingManagement/index.php
@@ -0,0 +1,143 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Parking</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="mvc/views/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="mvc/views/css/sb-admin-2.min.css" rel="stylesheet">
+    
+    <!-- Custom styles for background image-->
+    <style>
+        body {
+            background-image: url('');
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+        }
+        
+        .header {
+            text-align: center;
+            padding: 20px 0;
+            background-color: #4e73df; /* Thay đổi màu header tùy thích */
+            color: #ffffff; /* Màu chữ trắng */
+            font-size: 36px; /* Kích thước font chữ */
+            font-weight: bold; /* Đậm */
+        }
+    </style>
+
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Header -->
+                    <div class="header">
+                        Parking
+                    </div>
+                    <!-- End Header -->
+
+                    <!-- Content Row -->
+                    <div class="row justify-content-center mt-5">
+                        <!-- Workforce Card Example -->
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-success shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                                Chỗ trống ô tô khả dụng</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                                
+                                            </div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-car fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Content Row -->
+
+                    <!-- Content Row -->
+                    <div class="row justify-content-center mt-5">
+                        <!-- Workforce Card Example -->
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-success shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                                Chỗ trống xe máy khả dụng</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                                
+                                            </div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-motorcycle fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Content Row -->
+
+                    <!-- Login Button -->
+                    <div class="row justify-content-center mt-5">
+                        <a href="mvc/views/login.php" class="btn btn-primary">Đăng nhập</a>
+                    </div>
+                    <!-- End Login Button -->
+
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
